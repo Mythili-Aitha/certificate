7361e5e6a27512d40eb47583226d04a2a901d978
mighty <a@gmail.com>
2025-02-14 16:27:03 -0600

    lolo

--- src/main/java/net/project/certificate/controller/DocumentController.java
+++ src/main/java/net/project/certificate/controller/DocumentController.java
@@ -2,10 +2,17 @@ package net.project.certificate.controller;
 
 import net.project.certificate.dto.DocumentDto;
 import net.project.certificate.service.DocumentService;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,6 +20,7 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/documents")
 public class DocumentController {
     private DocumentService documentService;
+    private final String UPLOAD_DIR = "uploads/";
     public DocumentController(DocumentService documentService) {
         this.documentService = documentService;
     }
@@ -26,6 +34,25 @@ public class DocumentController {
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(uploadedDocs);
+    }
+
+    @GetMapping("/uploads/{fileName:.+}")
+    public ResponseEntity<Resource> getFile(@PathVariable String fileName) {
+        try {
+            Path filePath = Paths.get(UPLOAD_DIR).resolve(fileName);
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if (resource.exists() && resource.isReadable()) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.parseMediaType(Files.probeContentType(filePath)))
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
     @GetMapping("/certificate/{certificateId}")
